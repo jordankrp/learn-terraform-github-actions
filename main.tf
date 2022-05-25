a2684b4bdefc32327c09186cef27d6aa5b79ed4f
Jordan Karapanagiotis <jordan.krp.93@gmail.com>
2022-05-25 17:14:01 +0200

    Updated org

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG_Terraform_Demo"
+    organization = "ACG-Terraform-Github-Demo"
 
     workspaces {
       name = "demo-github-actions"
